39d70d4ba50b33478a6dfa022378b75f6e265fbe
Chaitanya Boda <chaitanya.boda@outlook.com>
2018-05-30 09:30:10 +0530

    Updated Readme.txt and removed details from settings.py

--- address_project/address_storage/settings.py
+++ address_project/address_storage/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -26,7 +25,6 @@ SECRET_KEY = '1h$$blhl-pnk^lyrsangrp&6dwwd=ho^*kj_k*=yikqcdyw9kt'
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -85,14 +83,13 @@ REST_FRAMEWORK = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'Sample',
-        'USER': 'postgres',
-        'PASSWORD': 'workjapan',
+        'NAME': '',  # db name
+        'USER': '',  # user name
+        'PASSWORD': '',  # password
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
@@ -112,7 +109,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
@@ -125,7 +121,6 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
